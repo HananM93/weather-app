9181783f291751d61f927fe4bc816a6ceee6796c
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-12-04 18:00:45 -0700

    add feedback

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Put your live link here
+
 # React Weather Icons
 [Weather Icons Deliverable](https://seir-830-yeti.netlify.app/react-fundamentals/week-12/day-3/labs/react-weather-forecast)<br/>
 To Get Started<br/><br/>
--- src/components/WeatherImage.js
+++ src/components/WeatherImage.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+// you are getting a warning to add an alt property for your image tag, i went in and add one
 const WeatherImage = ({img}) => {
     return (
-        <img src={img} />
+        <img src={img} alt='weather icon' />
     )
 }
 
